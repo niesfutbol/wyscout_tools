be2e9b4ea770b85016e108f176ed3f7175d8c2fd
Nepo Rojas <nepo.rojas@gmail.com>
2025-07-30 17:20:22 -0700

    ✅ Pass test

--- R/read_team_stats.R
+++ R/read_team_stats.R
@@ -8,3 +8,9 @@ read_team_stats <- function(path) {
   team <- readr::read_csv(path, show_col_types = FALSE, col_names = all_names$column_names, skip = 1)
   return(team)
 }
+
+#' @export
+read_team_stats_from_json <- function(path) {
+  team <- jsonlite::fromJSON(path)
+  return(team$matches)
+}
