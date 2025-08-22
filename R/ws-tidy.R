e800a2c751f763e92ab5ffddc4dd8e3f2e2b3a6c
Nepo Rojas <nepo.rojas@gmail.com>
2025-08-22 13:50:54 -0700

    ✅ Pass test

--- R/ws-tidy.R
+++ R/ws-tidy.R
@@ -8,3 +8,10 @@ obtain_passes_to_final_third_of_two_teams_by_match <- function(team_stats) {
     dplyr::group_by(Date) |>
     dplyr::summarize(total_passes_to_final_third = sum(Passes_to_final_third_accurate))
 }
+
+obtain_passes_to_final_third_of_two_teams_by_match.team_stats_from_json <- function(team_stats) {
+  team_stats$teamStats |>
+    dplyr::bind_rows(team_stats$opponentTeamStats) |>
+    dplyr::group_by(date) |>
+    dplyr::summarize(total_passes_to_final_third = sum(pass_to_final_third_success))
+}
