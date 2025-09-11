eee58c93b1d47922721c004c05c56fd838fb447c
Nepo Rojas <nepo.rojas@gmail.com>
2025-09-11 12:54:43 -0700

    🛑🧪 Test `add_tilt.team_stats_from_json()`

--- tests/testthat/test-add_column_tilt.R
+++ tests/testthat/test-add_column_tilt.R
@@ -13,4 +13,25 @@ describe("add_tilt", {
   )
   obtained <- add_tilt(raw_data)
   expect_equal(obtained, expected)
+
+  it("add_tilt.team_stats_from_json()", {
+    match <- tibble::tibble(date = c("2021-01-01", "2021-01-02", "2021-01-03"))
+    team_stats <- tibble::tibble(passToFinalThirdSuccess = c(1, 2, 3))
+    opponent_team_stats <- tibble::tibble(passToFinalThirdSuccess = c(2, 3, 4))
+    team_stats_from_json <- list(
+      match = match,
+      teamStats = team_stats,
+      opponentTeamStats = opponent_team_stats
+    )
+    class(team_stats_from_json) <- "team_stats_from_json"
+
+    expected <- tibble::tibble(
+      passToFinalThirdSuccess = c(1, 2, 3),
+	  date = c("2021-01-01", "2021-01-02", "2021-01-03"),
+	  total_passes_to_final_third = c(3, 5, 7),
+      tilt = c(100 * 1 / 3, 100 * 2 / 5, 100 * 3 / 7)
+    )
+    obtained <- add_tilt.team_stats_from_json(team_stats_from_json)
+    expect_equal(obtained, expected)
+  })
 })
